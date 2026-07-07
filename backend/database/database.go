16950b9a4d775198ef690d25697ae201cd5059b8
kanned1079 <kanned1079@gmail.com>
2026-07-07 20:12:54 +0800

    add pgdb support

--- backend/database/database.go
+++ backend/database/database.go
@@ -7,9 +7,9 @@ import (
 	"hy-board-backend/config"
 	"hy-board-backend/models"
 
+	"github.com/glebarez/sqlite"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
